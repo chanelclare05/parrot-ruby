69954c7763b1effa51c75ca2056999a5f76a9538
chanelclare05 <chaneldonahoe@hotmail.com>
2021-09-01 09:59:59 +1000

    done

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase = "Squawk!")
-    puts phrase
-    return phrase
+def parrot(sound = "Squawk!")
+    puts sound
+    return sound
 end
